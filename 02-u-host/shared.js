95323b52720f94ffc3271612df208aaff58e06f4
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2023-12-12 04:38:08 +0600

    16 - Transitions & Animations in CSS

--- 02-u-host/shared.js
+++ 02-u-host/shared.js
@@ -1,40 +1,57 @@
 var backdrop = document.querySelector(".backdrop");
-var selectPlanButtons = document.querySelectorAll(".plan button");
 var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener("click", function () {
-        // modal.style.display = "block";
-        // backdrop.style.display = "block";
         modal.classList.add("open");
-        backdrop.classList.add("open");
+        backdrop.style.display = "block";
+        setTimeout(function () {
+            backdrop.classList.add("open");
+        }, 10);
     });
 }
 
 backdrop.addEventListener("click", function () {
-    // mobileNav.style.display = "none";
     mobileNav.classList.remove("open");
     closeModal();
 });
 
-if (modalNoButton)
+if (modalNoButton) {
     modalNoButton.addEventListener("click", closeModal);
+}
 
 function closeModal() {
-    // modal.style.display = "none";
-    // backdrop.style.display = "none";
-    if (modal)
+    if (modal) {
         modal.classList.remove("open");
+    }
     backdrop.classList.remove("open");
+    setTimeout(function () {
+        backdrop.style.display = "none";
+    }, 200);
 }
 
 toggleButton.addEventListener("click", function () {
-    // mobileNav.style.display = "block";
-    // backdrop.style.display = "block";
     mobileNav.classList.add("open");
-    backdrop.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function () {
+        backdrop.classList.add("open");
+    }, 10);
+});
+
+ctaButton.addEventListener('animationstart', function (event) {
+    console.log('animation started', event)
+})
+
+ctaButton.addEventListener('animationend', function (event) {
+    console.log('animation ended', event)
+})
+
+ctaButton.addEventListener('animationitaration', function (event) {
+    console.log('animation itaration', event)
 })
